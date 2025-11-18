68c6d9f5a8cbaa84e9369f956d3e0192a95bec2f
uniabhi <uniabhi5684@gmail.com>
2025-11-18 16:24:57 +0530

    second version of dvc data saved

--- mycode.py
+++ mycode.py
@@ -9,6 +9,10 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
+# # Adding new row to df for V2
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
+
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
